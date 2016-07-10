1ad0e5563f3ca0a470aa82c3b4db2da3614ebf14
luisbalam <luisbalam@gmail.com>
2016-07-09 21:21:10 -0500

    Version ya actualizada del proyecto!!!

--- app/src/main/java/com/luisbalam/miprimerproyectoandroid_luisbalam/MainActivity.java
+++ app/src/main/java/com/luisbalam/miprimerproyectoandroid_luisbalam/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Autor: Luis Alberto Balam Mukul (curso Android UNAM)
     }
 }
